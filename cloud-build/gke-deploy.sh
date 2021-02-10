04f70b2ebd22529480b004bc8c0d3f42d1281ffc
pulkit <pulkit632@gmail.com>
2021-02-10 04:08:22 +0000

    Enabling the pod horizontal autoscaling and making the script sleep for 3m in order for load balancer provisioning

--- cloud-build/gke-deploy.sh
+++ cloud-build/gke-deploy.sh
@@ -4,6 +4,14 @@ gcloud container clusters get-credentials example-private-cluster --region=austr
 
 kubectl create deployment go-app-deploy --image=gcr.io/servian-tech/goapp-image:latest
 
+#Horizontal Pod Autoscaler
+kubectl autoscale deployment go-app-deploy --cpu-percent=80 --min=1 --max=5
+
 #Load balancer for public exposure
 
 kubectl expose deployment go-app-deploy --type=LoadBalancer --port 80 --target-port 3000
+
+sleep 3m
+echo "Waiting for Load Balancer to be provisioned"
+#Service Details
+kubectl get service

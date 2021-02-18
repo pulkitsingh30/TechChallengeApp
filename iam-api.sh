7cc06afc4dc60d61dce7559154b724b68165a91a
pulkit <pulkit632@gmail.com>
2021-02-18 03:22:50 +0000

    Updating script to get the variable Project number

--- iam-api.sh
+++ iam-api.sh
@@ -1,16 +1,19 @@
-gcloud services enable cloudresourcemanager.googleapis.com cloudbuild.googleapis.com logging.googleapis.com compute.googleapis.com container.googleapis.com iam.googleapis.com storage.googleapis.com
-
 PROJECT_ID=$(gcloud config get-value project)
-gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:910704256954@cloudbuild.gserviceaccount.com --role=roles/compute.networkAdmin
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:${PROJECT_ID}"  --format='value(project_number)')
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/compute.networkAdmin
 
-gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:910704256954@cloudbuild.gserviceaccount.com --role=roles/compute.securityAdmin
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/compute.securityAdmin
 
-gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:910704256954@cloudbuild.gserviceaccount.com --role=roles/container.admin
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/container.admin
 
-gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:910704256954@cloudbuild.gserviceaccount.com --role=roles/iam.securityAdmin
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/iam.securityAdmin
 
-gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:910704256954@cloudbuild.gserviceaccount.com --role=roles/iam.serviceAccountCreator
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/iam.serviceAccountCreator
 
-gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:910704256954@cloudbuild.gserviceaccount.com --role=roles/iam.serviceAccountUser
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/iam.serviceAccountUser
 
-gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:910704256954@cloudbuild.gserviceaccount.com --role=roles/storage.admin
+gcloud projects add-iam-policy-binding $PROJECT_ID --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/storage.admin
+
+sleep 1m
+
+gcloud services enable cloudresourcemanager.googleapis.com cloudbuild.googleapis.com logging.googleapis.com compute.googleapis.com container.googleapis.com iam.googleapis.com storage.googleapis.com
